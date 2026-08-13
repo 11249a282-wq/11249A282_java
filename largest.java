024bbb4160fdc37caec868baa03ccfee7fbf9338
11249a282-wq <11249a282@kanchiuniv.ac.in>
2026-08-13 14:51:23 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class largest
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the first no");
+a=s.nextInt();
+System.out.println("enter the second no");
+b=s.nextInt();
+if(a>b)
+{
+System.out.println("largest number is:"+a);
+}
+else
+System.out.println("largest number is:"+b);
+}
+}
+
+
+
+                   
