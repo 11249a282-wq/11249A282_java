25f66d066e0b0d914e3666c4ea41e82eb40b76ad
11249a282-wq <11249a282@kanchiuniv.ac.in>
2026-08-20 14:41:39 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,10 @@
-import java.util.Scanner;
+
+
+import studentinfo.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first employee name: ");
-        String emp1 = sc.nextLine();
-
-        System.out.print("Enter second employee name: ");
-        String emp2 = sc.nextLine();
-
-        if (emp1.equalsIgnoreCase(emp2)) {
-            System.out.println("Both employees are working in the same department.");
-        } else {
-            System.out.println("Employees are working in different departments.");
-        }
-
-        sc.close();
+        Student s = new Student();
+        s.displayInfo();
     }
 }
